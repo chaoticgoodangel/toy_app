e351faeda36a2ce7bc7aad67fbdae532a55ff0e4
chaoticgoodangel <pereza4044@gmail.com>
2021-10-25 15:21:45 +0000

    config host for aws to prevent block

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "a202e4c366c744249f022990218bc8e5.vfs.cloud9.us-east-2.amazonaws.com"
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
